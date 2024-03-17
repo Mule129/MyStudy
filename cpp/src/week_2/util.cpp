25afed591eca79dc86d2d14c31fcb907e2a97efc
Mule129 <mule01129@gmail.com>
2024-03-18 01:36:59 +0900

    [Fix] Fixed random int bug

--- cpp/src/week_2/util.cpp
+++ cpp/src/week_2/util.cpp
@@ -1,13 +1,14 @@
 #include <string>
 #include <iostream>
 #include <random>
+#include <time.h>
 
 using namespace std;
 
 string random(int len) {
-    random_device rd;
+    // random_device rd;
 
-    mt19937 gen(rd());
+    mt19937 gen(time(NULL));
     uniform_int_distribution<int> dis(0, 9);
 
     int randomValueList[len];
