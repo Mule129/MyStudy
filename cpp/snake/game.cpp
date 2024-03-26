d7167c8bff4fb251aac9a5cc10c1bf924678f8f6
Mule129 <mule01129@gmail.com>
2024-03-26 16:38:28 +0900

    [Fixed] Apple add

--- cpp/snake/game.cpp
+++ cpp/snake/game.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <random>
 #include <time.h>
+#include <iostream>
 
 #include "console.h"
 
@@ -19,9 +20,14 @@
 int randInt(int min, int max);
 
 int x, y;
+int tailX, tailY;
+int appleX;
+int appleY;
+int score;
 int werstInput;
-int snakeArray[BOARD_SIZE*BOARD_SIZE][1] = {0};
-int snakeNotArray[BOARD_SIZE*BOARD_SIZE][1] = {0};
+int snakeArrayX[BOARD_SIZE] = {0};
+int snakeArrayY[BOARD_SIZE] = {0};
+// int snakeNotArray[BOARD_SIZE*BOARD_SIZE][1] = {0};
 int snakeLength;
 
 
@@ -54,15 +60,57 @@ int handleInput() {
     return werstInput;
 }
 
+void screenCleaer() {
+    console::clear(1, console::SCREEN_WIDTH-1, 1, console::SCREEN_HEIGHT-1);
+}
+
+
+
+void _apple() {
+    appleX = randInt(1, BOARD_SIZE-1);
+    appleY = randInt(1, BOARD_SIZE-1);
+    console::draw(appleX, appleY, APPLE_STRING);
+}
+
+void eatApple() {
+    snakeLength++;
+    score += 10;
+    console::draw(appleX, appleY, SNAKE_BODY_STRING);
+    _apple();
+    
+}
+
+void _snakePositionSave(int x, int y, int snakeLength) {
+    if (snakeLength = 1) {
+        snakeArrayX[0] = x;
+        snakeArrayY[0] = y;
+        tailX = x;
+        tailY = y;
+    } else {
+        for (int i = snakeLength - 1; i >= 0; i--) {
+            snakeArrayX[i+1] = snakeArrayX[i];
+            snakeArrayY[i+1] = snakeArrayY[i];
+        }
+        tailX = snakeArrayX[snakeLength-1];
+        tailY = snakeArrayY[snakeLength-1];
+    }
+
+    std::cout << snakeArrayX[1];
+
+    snakeArrayX[0] = x;
+    snakeArrayY[0] = y;
+    
+}
+
 void initGameSetting() {
     x = BOARD_SIZE/2;
     y = BOARD_SIZE/2;
     
     snakeLength = 1;
-    snakeArray[0][0] = x;
-    snakeArray[0][1] = y;
+    _snakePositionSave(x, y, snakeLength);
 
-    int snakeArray[BOARD_SIZE*BOARD_SIZE][1] = {0};
+    score = 0;
+    _apple();
 
     werstInput = 2;
 
@@ -74,38 +122,7 @@ void initGameSetting() {
     console::draw(x, y, SNAKE_STRING);
 }
 
-void screenCleaer() {
-    console::clear(1, console::SCREEN_WIDTH-1, 1, console::SCREEN_HEIGHT-1);
-}
-
-void snakePositionSave(int x, int y) {
-    for (int i = snakeLength - 1; i > 0; i--) {
-        snakeArray[snakeLength+1][0] = snakeArray[snakeLength][0];
-        snakeArray[snakeLength+1][1] = snakeArray[snakeLength][1];
-    }
-
-    snakeArray[0][0] = x;
-    snakeArray[0][1] = y;
-    
-}
-
-bool eatApple(int x, int y) {
-    if (1){
-        console::draw(x, y, SNAKE_BODY_STRING);
-        x = randInt(0, BOARD_SIZE);
-        y = randInt(0, BOARD_SIZE);
-        for (int i = 0; i < snakeLength; i++) {
-            
-        }
-        console::draw(x, y, APPLE_STRING);
-        return true;
-    } else {
-
-    }
-
-}
-
-bool snakeHeadCheck() {
+bool snakeHeadCheck(int x, int y) {
     if (x >= BOARD_SIZE || y >= BOARD_SIZE || x <= 0 || y <= 0) {
         console::draw(
             BOARD_SIZE/2 - 4, BOARD_SIZE/2, "YOU LOST!"
@@ -114,23 +131,36 @@ bool snakeHeadCheck() {
             BOARD_SIZE/2 - 9, BOARD_SIZE/2+1, "Try again? (Enter)"
         );
         return true;
-    } else if (eatApple(x, y)) {
-        snakeLength++;
+    } else if (x == appleX && y == appleY) {
+        eatApple();
+    } else {
+        for (int i = 1; i < snakeLength; i++) {
+            for (int j = 1; j < snakeLength; j++) {
+                if (snakeArrayX[i] == x && snakeArrayY[i] == y) {
+                    console::draw(
+                        BOARD_SIZE/2 - 4, BOARD_SIZE/2, "YOU LOST!"
+                    );
+                    console::draw(
+                        BOARD_SIZE/2 - 9, BOARD_SIZE/2+1, "Try again? (Enter)"
+                    );
+                    console::draw(
+                        BOARD_SIZE/2 - 4, BOARD_SIZE/2+2,
+                        std::to_string(snakeArrayX[i])
+                    );
+                    return true;
+                }
+            }
+        }
     }
+
+    _snakePositionSave(x, y, snakeLength);
     
     return false;
 }
 
 
-
-void controll() {
-
-}
-
-
 int game() {
     int delayCnt = 0;
-    int score = 0;
     bool lossGame = false;
     std::string scoreText = "score: ";
 
@@ -140,8 +170,7 @@ int game() {
     initGameSetting();
 
     while (true) {
-        // console::clear(x, y);
-        console::draw(x, y, SNAKE_BODY_STRING);
+        console::clear(tailX, tailY);
 
         int status = handleInput();
 
@@ -172,17 +201,18 @@ int game() {
             y++;
         }
         
+        lossGame = snakeHeadCheck(x, y);
+        console::draw(x, y, SNAKE_BODY_STRING);
         
         console::draw(0, BOARD_SIZE + 1, "x: " + std::to_string(x));
         console::draw(0, BOARD_SIZE + 2, "y: " + std::to_string(y));
-        console::draw(0, BOARD_SIZE + 4, "input: " + std::to_string(werstInput));
+        console::draw(0, BOARD_SIZE + 4, "sanke: " + std::to_string(tailX));
         console::draw(
             (BOARD_SIZE / 2) - (scoreText + std::to_string(score)).length()/2, 
             BOARD_SIZE + 1, 
             "score: " + std::to_string(score)
         );
 
-        lossGame = snakeHeadCheck();
         
         console::wait();
     }   
